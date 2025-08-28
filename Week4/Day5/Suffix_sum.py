0a9c1007361e0967395a38a518ba1a0f2afa5d6d
Rajashri Roy Choudhury <rajashri.biology@gmail.com>
2025-08-28 12:38:53 +0530

    Suffix_sum.py

---
+++ Week4/Day5/Suffix_sum.py
@@ -0,0 +1,7 @@
+def suffix_sum(arr):
+    n = len(arr)
+    suffix = [0] * n
+    suffix[n-1] = arr[n-1]
+    for i in range(n-2, -1, -1):
+        suffix[i] = suffix[i+1] + arr[i]
+    return suffix
