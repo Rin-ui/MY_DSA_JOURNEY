7f82d5fe20ff629c48468f3feee1fc6eaa1fe226
Rajashri Roy Choudhury <rajashri.biology@gmail.com>
2026-01-08 11:24:29 +0530

    prime_check.py

---
+++ 8_jan_revise/prime_check.py
@@ -0,0 +1,12 @@
+# check prime number
+n = int(input("enter a number: "))
+
+if n < 2:
+    print("not prime")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("not prime")
+            break
+    else:
+        print("prime")
