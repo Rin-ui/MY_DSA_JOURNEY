5ef316422b9b7b4b35356d70c729220f2b1b6e2b
Rajashri Roy Choudhury <rajashri.biology@gmail.com>
2026-01-08 12:24:47 +0530

    palindrome.py

---
+++ 8_jan_revise/palindrome.py
@@ -0,0 +1,11 @@
+n = int(input("enter a number: "))
+rev = 0
+temp = n
+while n > 0:
+    rev = rev * 10 + (n % 10)
+    n = n // 10
+
+if temp == rev:
+    print("palindrome")
+else:
+    print("not palindrome")
