b4929515f4c550e609cddab013a2c2b0aa18c9f1
Rajashri Roy Choudhury <rajashri.biology@gmail.com>
2026-01-11 16:58:40 +0530

    running_sum.py

--- Revise/Array/prefix_sum/running_sum.py
+++ Revise/Array/prefix_sum/running_sum.py
@@ -2,6 +2,6 @@
 nums = list(map(int, input().split()))
 prefix = [0] * len(nums)
 prefix[0] = nums[0]
-for i in range(len(nums)):
+for i in range(1,len(nums)):
     prefix[i] = prefix[i-1] + nums[i]
 print(prefix)
