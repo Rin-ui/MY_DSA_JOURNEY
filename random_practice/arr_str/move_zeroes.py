da96c6d75d55003d4fbce47f83d49065c5771e27
Rajashri Roy Choudhury <rajashri.biology@gmail.com>
2026-03-31 18:47:25 +0530

    move_zeroes.py

---
+++ random_practice/arr_str/move_zeroes.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int,input().split()))
+count = 0
+new_arr = []
+for i in range(len(arr)):
+    if arr[i] ==0:
+        count +=1
+        continue
+    else:
+        new_arr.append(arr[i])
+for i in range(count):
+    new_arr.append(0)
+print(new_arr)
